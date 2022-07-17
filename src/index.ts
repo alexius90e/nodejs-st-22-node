441bc330a4e4cbc9eae52e436a1577faaafa89f4
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2022-07-17 15:32:02 +0300

    Task-2-2: Refactored error handler & added params to csvtojson

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,25 @@ import * as fs from 'fs';
 import * as csvtojson from 'csvtojson';
 
 const csvFilePath = './csv/nodejs-hw1-ex1.csv';
-const readableStream = fs.createReadStream(csvFilePath).pipe(csvtojson());
-const writableStream = fs.createWriteStream('./src/result.txt');
+const csvtojsonParams = {
+  checkType: true,
+  headers: ['book', 'author', 'amount', 'price'],
+};
 
-readableStream.on('data', (chunk: Buffer): void => {
-  writableStream.write(chunk.toString());
-});
+const writableStream = fs.createWriteStream('./csv/result.txt');
 
-readableStream.on('error', (error: Error): void => console.error(error.message));
+const readableStream = fs.createReadStream(csvFilePath);
 
-writableStream.on('ready', () => console.log('The file is ready'));
+readableStream
+  .pipe(csvtojson(csvtojsonParams))
+  .on('data', (chunk: Buffer): void => {
+    writableStream.write(chunk.toString());
+  });
+
+readableStream.on('error', (error: Error): void =>
+  console.error(error.message),
+);
+
+writableStream.on('ready', () =>
+  console.log('The file is ready, path: "./csv/result.txt"'),
+);
